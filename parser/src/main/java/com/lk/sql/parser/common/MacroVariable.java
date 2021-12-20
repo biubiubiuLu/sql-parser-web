b40cdc55775674f68d0df4f93f79ef4e22bc097e
kai.lu <kai.lu@ly.com>
2021-12-20 22:21:37 +0800

    IdentifierSegment 中添加语法: ${宏变量名}

---
+++ parser/src/main/java/com/lk/sql/parser/common/MacroVariable.java
@@ -0,0 +1,22 @@
+package com.lk.sql.parser.common;
+
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlWriter;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.sql.util.SqlVisitor;
+import org.apache.calcite.sql.validate.SqlValidator;
+import org.apache.calcite.sql.validate.SqlValidatorScope;
+import org.apache.calcite.util.Litmus;
+
+import java.util.Objects;
+
+/**
+ * @author lukai
+ * @creare 2021-12-20 21:24
+ */
+public class MacroVariable  {
+
+    public MacroVariable(SqlParserPos pos) {
+    }
+
+}
